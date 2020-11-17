732251ede1ab72b1e5071c756d4018611a27e1b4
chdaud78 <chdaud36@gmail.com>
2020-11-17 17:56:04 +0900

    [ADD] Lotto

---
+++ 6.로또추첨기/LottoBall.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="ball" :style="styleObject" > {{number}}</div>
+</template>
+
+<script>
+export default {
+  name: 'LottoBall',
+  props: {
+    number: Number,
+  }, //props로 자식이 받은 값은 바꿀수 없음. read-only
+  computed: {
+    styleObject() {
+    let background;
+    if(this.number <= 10) {
+      background= 'red';
+    } else if (this.number <= 20) {
+      background = 'orange';
+    } else if (this.number <= 30) {
+      background = 'yellow';
+    } else if (this.number <= 40) {
+      background = 'blue';
+    } else {
+      background = 'green';
+    }
+    return {
+      background,
+    };
+  },
+  }
+};
+</script>
+
+<style>
+.ball {
+  display : inline-block;
+  border: 1px solid black;
+  border-radius: 20px;
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+  font-size: 20px;
+  text-align: center;
+  margin-right: 20px;
+}
+</style>
--- 6.로또추첨기/LottoGenerate.vue
+++ 6.로또추첨기/LottoGenerate.vue
@@ -1,29 +1,79 @@
 <template>
   <div>
-
+    <div>당첨결과</div>
+    <lotto-ball v-for="ball in winBall" :key="ball" :number="ball"></lotto-ball>
+    <div>보너스</div>
+    <lotto-ball v-if="bonusBall" :number="bonusBall"></lotto-ball>
+    <button v-if="redo" @click="onclickRedo">한번더</button>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
+import LottoBall from "./LottoBall";
 
-      };
+function getWinNumbers() {
+  const candidate = Array(45).fill().map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber];
+}
+const timeouts = [];
+export default {
+  components: {
+    'lotto-ball': LottoBall, //앞과 뒤의 pascal case, Kebab case가 같을 경우 서로 호환되어 LottoBall로 줄일수 있음.
+  },
+  data() {
+    return {
+      winNumbers: getWinNumbers(),
+      winBall: [],
+      bonusBall: null,
+      redo: false,
+    };
+  },
+  computed: {},
+  methods: {
+    onclickRedo() {
+      this.winNumbers = getWinNumbers();
+      this.winBall = [];
+      this.bonusBall = null;
+      this.redo = false;
+      this.showBalls();
     },
-    computed: {
-
-    },
-    methods: {
-
-    },
-  };
+    showBalls() {
+      for (let i = 0; i < this.winNumbers.length - 1; i++) {
+        timeouts[i] = setTimeout(() => {
+          this.winBall.push(this.winNumbers[i]);
+        }, (i + 1) * 1000);
+      }
+      timeouts[6] = setTimeout(() => {
+        this.bonusBall = this.winNumbers[6];
+        this.redo = true;
+      }, 7000);
+    }
+  },
+  mounted() {
+    this.showBalls();
+  },
+  beforeDestroyed() {
+    timeouts.forEach((t) => {
+      clearTimeout(t);
+    });
+  },
+  // watch: {
+  //   winBalls(value, oldValue) {
+  //     console.log(value, oldValue);
+  //     if (value.length === 0) {
+  //       this.showBalls();
+  //     }
+  //   }
+  // } 감시 역할. 최대한 사용 자제하기.
+};
 </script>
 
 <style>
-  #computer {
-    width: 142px;
-    height: 200px;
-    background-position: 0 0;
-  }
+
 </style>

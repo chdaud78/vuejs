56d53fcb5ae175a2e6984d53c1ef412d190dbc9b
chdaud78 <qkrri56@naver.com>
2020-11-24 10:17:20 +0900

    [ADD] 14. component

---
+++ vue practice/14.Component/app.js
@@ -0,0 +1,52 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            toggleCheck: false,
+            friends: [
+
+                {
+                    id: 'Julie',
+                    name: 'Julie Jones',
+                    phone: '09876 543 221',
+                    email: 'julie@localhost.com',
+                }
+            ]
+        };
+    },
+    methods: {
+
+    }
+});
+
+app.component('friend-contact', {
+    template: `
+    <li v-for="friend in friends" :key="friend.id">
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetail">
+        {{ toggleCheck ? 'Hide' : 'Show' }}Details</button>
+        <ul v-if="toggleCheck">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+    `,
+    data() {
+        return {
+            toggleCheck: false,
+            friends: [{
+            id: 'Manuel',
+                name: 'Manuel Lorenz',
+                phone: '01234 5678 991',
+                email: 'manuel@localhost.com',
+            }],
+        };
+    },
+    methods: {
+        toggleDetail() {
+            this.toggleCheck = !this.toggleCheck;
+        }
+    }
+
+});
+
+app.mount('#app')

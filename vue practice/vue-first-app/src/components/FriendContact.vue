da7056806717e02267d806dbf0e9bff2648aaf0c
chdaud78 <qkrri56@naver.com>
2020-11-24 13:28:31 +0900

    [ENH] styling, add content

--- vue practice/vue-first-app/src/components/FriendContact.vue
+++ vue practice/vue-first-app/src/components/FriendContact.vue
@@ -1,11 +1,37 @@
-<template></template>
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetail">
+     {{toggleCheck ? 'Hide' : 'Show'}} Details
+    </button>
+    <ul v-if="toggleCheck">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
 
 <script>
 export default {
   data() {
-
+    return {
+      toggleCheck: false,
+      friend: {
+        id: 'jea',
+        name: 'Park Jaehyun',
+        phone: '010 1234 5678',
+        email: 'jae@naver.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetail() {
+      this.toggleCheck = !this.toggleCheck;
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+
+</style>

dd9adb03d249fadac2cbf2b33e6d44092cf24abe
chdaud78 <qkrri56@naver.com>
2020-11-25 17:53:47 +0900

    [ADD] vue-second-app

--- vue practice/vue-second-app/src/main.js
+++ vue practice/vue-second-app/src/main.js
@@ -1,13 +1,12 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-
-import BaseBadge from './components/UI/BaseBadge.vue';
 import BaseCard from './components/UI/BaseCard.vue';
+import BaseButton from './components/UI/BaseButton.vue';
 
 const app = createApp(App);
 
-app.component('base-badge', BaseBadge);
 app.component('base-card',BaseCard);
+app.component('base-button',BaseButton);
 
 app.mount('#app');

8990ca168e3c75d7dd5088d5eb1839a81b9bb0c6
chdaud78 <qkrri56@naver.com>
2020-11-27 09:33:48 +0900

    [ENH] Button

--- vue practice/vue-second-app/src/App.vue
+++ vue practice/vue-second-app/src/App.vue
@@ -1,34 +1,17 @@
 <template>
   <the-header title="Remember Me"></the-header>
   <stored-resource :resources="storedResources"></stored-resource>
+  <the-resource></the-resource>
 </template>
 
 <script>
-import StoredResource from "./components/learning-resource/StoredResource.vue"
 import TheHeader from "./components/layouts/TheHeader.vue"
+import TheResource from "./components/learning-resource/TheResource.vue"
 
 export default {
   components: {
-    StoredResource,
     TheHeader,
-  },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Let\'s learn about google',
-          link: 'https://google.com'
-        },
-      ]
-    };
+    TheResource,
   },
 };
 </script>

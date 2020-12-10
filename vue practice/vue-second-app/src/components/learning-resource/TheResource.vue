8d82c1a2f71a3b714de967cc1e09b2b265d61d51
chdaud78 <qkrri56@naver.com>
2020-12-10 18:00:33 +0900

    [ADD]

--- vue practice/vue-second-app/src/components/learning-resource/TheResource.vue
+++ vue practice/vue-second-app/src/components/learning-resource/TheResource.vue
@@ -5,7 +5,9 @@
     >Stored Resources</base-button>
     <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode">Add Resource</base-button>
   </base-card>
+  <keep-alive>
   <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -47,12 +49,23 @@ export default {
   provide() {
     return {
       resources: this.storedResources,
+      addResource: this.addResource,
     };
   },
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
     },
+    addResource(title,description,url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description : description,
+        link: url,
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources'
+    }
   },
 }
 </script>
